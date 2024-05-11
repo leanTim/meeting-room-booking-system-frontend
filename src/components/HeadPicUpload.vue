c1104c851a623d39d95a493717de4fbdd259e78c
leanTim <zhu3521953@163.com>
2024-05-11 10:05:30 +0800

    feat: 更新用户信息包括头像上传修改

---
+++ src/components/HeadPicUpload.vue
@@ -0,0 +1,79 @@
+<template>
+    {{ headPic }}
+    {{ picUrl }}
+    <div id="upload-component-container">
+        <el-upload class="avatar-uploader" action="http://localhost:3001/user/upload"
+            :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+            <img v-if="!imageUrl"  :src="picUrl" alt="" class="avatar">
+            <img v-if="imageUrl" :src="picUrl" class="avatar" />
+            <el-icon v-else class="avatar-uploader-icon">
+                <Plus />
+            </el-icon>
+        </el-upload>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import { computed, ref, toRef } from 'vue'
+import { ElMessage } from 'element-plus'
+import { Plus } from '@element-plus/icons-vue'
+import type { UploadProps } from 'element-plus'
+
+
+const props = defineProps(['headPic'])
+const emit = defineEmits(['update-pic'])
+
+const imageUrl = ref(props.headPic)
+console.log(imageUrl.value)
+const picUrl = computed(() => `http://localhost:3001/${props.headPic}`)
+
+const handleAvatarSuccess: UploadProps['onSuccess'] = (
+    response,
+    uploadFile
+) => {
+    imageUrl.value = response.data
+    emit('update-pic', imageUrl.value)
+}
+const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
+    // console.log(rawFile)
+    if (rawFile.type !== 'image/png') {
+        ElMessage.error('Avatar picture must be JPG format!')
+        return false
+    } else if (rawFile.size / 1024 / 1024 > 3) {
+        ElMessage.error('Avatar picture size can not exceed 2MB!')
+        return false
+    }
+    return true
+}
+</script>
+
+<style scoped>
+.avatar-uploader .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+}
+</style>
+<style>
+.avatar-uploader .el-upload {
+    border: 1px dashed var(--el-border-color);
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+    border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    text-align: center;
+}
+</style>
--- src/components/UpdateUserInfo.vue
+++ src/components/UpdateUserInfo.vue
@@ -7,18 +7,19 @@
             >
 
             <el-form-item 
+                label="头像"
+            >
+                <HeadPicUpload
+                    :headPic="userInfo.headPic"
+                    @update-pic="updatePic"
+                ></HeadPicUpload>
+            </el-form-item>
+
+            <el-form-item 
                 label="昵称："   
             >
                 <el-input 
                 v-model="userInfo.nickName"
-                ></el-input>
-            </el-form-item>
-
-            <el-form-item 
-                label="头像"
-            >
-                <el-input 
-                v-model="userInfo.headPic"
                 ></el-input>
             </el-form-item>
 
@@ -64,6 +65,8 @@
 import { getUserInfo, updateUserInfo } from '@/interface/interfaces';
 import { ElMessage } from 'element-plus';
 import { onMounted, reactive } from 'vue';
+import HeadPicUpload from './HeadPicUpload.vue';
+
 
 export interface updateUserInfoType {
     username: string
@@ -89,17 +92,23 @@ const getInfo = async function () {
     const res = await getUserInfo()
     const data = res.data.data
 
-    userInfo.nickName = data.nickName
+    userInfo.username = data.username
+    userInfo.nickName = data.nickName || ''
     userInfo.headPic = data.headPic || ''
     userInfo.email = data.email || ''
-
+    // console.log(userInfo)
     return res
+}
+
+function updatePic(url: string) {
+    console.log(url, '-------------url---------')
+    userInfo.headPic = url
 }
 
 async function onUpdateUserInfo (data: updateUserInfoType) {
     const res = await updateUserInfo(data)
     
-    console.log(res)
+    // console.log(res)
     if(res.status ===200 || res.status === 201) {
         const {message: msg, data} = res.data
 
