fcee57de5e2ae0c10c0c6779996fa9d0fdec2135
leanTim <zhu3521953@163.com>
2024-05-11 17:30:31 +0800

    feat: 菜单栏完成

--- src/components/ManageMenu.vue
+++ src/components/ManageMenu.vue
@@ -1,59 +1,71 @@
 <template>
-  <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
-    <el-radio-button :value="false">expand</el-radio-button>
-    <el-radio-button :value="true">collapse</el-radio-button>
-  </el-radio-group>
-  <el-menu
-    default-active="2"
-    class="el-menu-vertical-demo"
-    :collapse="isCollapse"
-    @open="handleOpen"
-    @close="handleClose"
-  >
-    <el-menu-item index="1">
-      <el-icon><icon-menu /></el-icon>
-      <template #title>个人信息</template>
-    </el-menu-item>
-    <el-menu-item index="2">
-      <el-icon><icon-menu /></el-icon>
-      <template #title>会议室管理</template>
-    </el-menu-item>
-    <el-menu-item index="3" disabled>
-      <el-icon><document /></el-icon>
-      <template #title>预定管理</template>
-    </el-menu-item>
-    <el-menu-item index="4">
-      <el-icon><setting /></el-icon>
-      <template #title>用户管理</template>
-    </el-menu-item>
-    <el-menu-item index="5">
-      <el-icon><setting /></el-icon>
-      <template #title>统计</template>
-    </el-menu-item>
-  </el-menu>
+    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
+        <el-radio-button :value="false">展开</el-radio-button>
+        <el-radio-button :value="true">折叠</el-radio-button>
+    </el-radio-group>
+    <el-menu default-active="2" class="el-menu-vertical-demo" :collapse="isCollapse" @open="handleOpen"
+        @close="handleClose">
+        <el-menu-item index="1">
+            <el-icon><icon-menu /></el-icon>
+            <template #title>
+                <router-link to="update_info">个人信息</router-link>
+            </template>
+        </el-menu-item>
+        <el-menu-item index="2">
+            <el-icon><icon-menu /></el-icon>
+            <template #title>
+                <router-link to="meeting_room_manage"> 会议室管理</router-link>
+            </template>
+        </el-menu-item>
+        <el-menu-item index="3">
+            <el-icon>
+                <document />
+            </el-icon>
+            <template #title>
+                <router-link to="booking_manage">预定管理</router-link>
+            </template>
+        </el-menu-item>
+        <el-menu-item index="4">
+            <el-icon>
+                <setting />
+            </el-icon>
+            <template #title>
+                <router-link to="user_manage">用户管理</router-link>
+            </template>
+        </el-menu-item>
+        <el-menu-item index="5">
+            <el-icon>
+                <setting />
+            </el-icon>
+            <template #title>
+                <router-link to="statistics">统计</router-link>
+            </template>
+        </el-menu-item>
+    </el-menu>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
+import router from '../router/index';
 import {
-  Document,
-  Menu as IconMenu,
-  Location,
-  Setting,
+    Document,
+    Menu as IconMenu,
+    Location,
+    Setting,
 } from '@element-plus/icons-vue'
 
-const isCollapse = ref(true)
+const isCollapse = ref(false)
 const handleOpen = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
+    console.log(key, keyPath)
 }
 const handleClose = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
+    console.log(key, keyPath)
 }
 </script>
 
 <style>
 .el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
-  min-height: 400px;
+    width: 200px;
+    min-height: 400px;
 }
 </style>
